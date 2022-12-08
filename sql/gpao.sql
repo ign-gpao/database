4fe4983cab405501673740070e74bd381d5eb589
Arnaud Birk <arnaud.birk@ign.fr>
2022-12-08 11:02:28 +0100

    fix create database #patch

--- sql/gpao.sql
+++ sql/gpao.sql
@@ -17,6 +17,28 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: gpao; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE gpao WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE gpao OWNER TO postgres;
+
+\connect gpao
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: priority; Type: TYPE; Schema: public; Owner: postgres
 --
 
